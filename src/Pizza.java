ff5dac38a92f97870eca8c63cc183e843e5c734f
kenkl <kik5429@psu.edu>
2026-03-31 22:08:13 -0400

    coding for pizza class and sender

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,63 @@
+/**
+ * Project: Lab 4
+ * Purpose Details: Pizza class for sending and receiving pizza data
+ * Course: IST 242
+ * Author: Ken Klos
+ * Date Developed:
+ * Last Date Changed:
+ * Rev:
+ */
 public class Pizza {
+
+    private String size;
+    private String crust;
+    private String toppings;
+    private double price;
+
+    public Pizza() {
+    }
+
+    public Pizza(String size, String crust, String toppings, double price) {
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCrust() {
+        return crust;
+    }
+
+    public void setCrust(String crust) {
+        this.crust = crust;
+    }
+
+    public String getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return size + "," + crust + "," + toppings + "," + price;
+    }
 }
--- src/PizzaSender.java
+++ src/PizzaSender.java
@@ -1,2 +1,54 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/*
+  Project: Lab 4
+  Purpose Details: Sends flat file and JSON pizza messages
+  Course: IST 242
+  Author: Ken Klos
+  Date Developed:
+  Last Date Changed:
+  Rev:
+ */
 public class PizzaSender {
+
+    private static final String FLAT_QUEUE = "pizzaFlatQueue";
+    private static final String JSON_QUEUE = "pizzaJsonQueue";
+
+    public static void main(String[] args) throws Exception {
+
+        // Create pizza object
+        Pizza pizza = new Pizza("Large", "Thin Crust", "Pepperoni", 12.99);
+
+        // Flat file message
+        String flatMessage = pizza.toString();
+
+        // JSON message
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonMessage = mapper.writeValueAsString(pizza);
+
+        // RabbitMQ setup
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        // Create queues
+        channel.queueDeclare(FLAT_QUEUE, false, false, false, null);
+        channel.queueDeclare(JSON_QUEUE, false, false, false, null);
+
+        // Send flat file message
+        channel.basicPublish("", FLAT_QUEUE, null, flatMessage.getBytes("UTF-8"));
+        System.out.println("Sent flat file message: " + flatMessage);
+
+        // Send JSON message
+        channel.basicPublish("", JSON_QUEUE, null, jsonMessage.getBytes("UTF-8"));
+        System.out.println("Sent JSON message: " + jsonMessage);
+
+        channel.close();
+        connection.close();
+    }
 }
